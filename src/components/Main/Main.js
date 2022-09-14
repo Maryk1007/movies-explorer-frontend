6d34e05b76d893d5c32a4092d7b4840eacfff479
Maria <mary-sberbank@yandex.ru>
2022-09-14 23:51:04 +0300

    layout about-project

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Promo from '../Promo/Promo';
-// import AboutProject from '../AboutProject/AboutProject';
+import AboutProject from '../AboutProject/AboutProject';
 // import Techs from '../Techs/Techs';
 // import AboutMe from '../AboutMe/AboutMe';
 // import Portfolio from '../Portfolio/Portfolio'
@@ -8,8 +8,8 @@ function Main() {
   return (
     <main className="content">
       <Promo/>
-      {/* <AboutProject/>
-      <Techs/>
+      <AboutProject/>
+      {/* <Techs/>
       <AboutMe/>
       <Portfolio/> */}
     </main>
